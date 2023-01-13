6ec02c903faad271b0f52a5e693f4efee95af27c
chris <chris@bumblehead.com>
2023-01-12 22:11:59 -0800

    added more details to qutebrowser/config.py

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,42 @@
 config.load_autoconfig(False)
 
+# c.qt.highdpi = True
+c.colors.webpage.bg = "black"
+# c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.threshold.background = 205
+
+c.fonts.completion.category = 'bold 14pt monospace'
+c.fonts.completion.entry = '14pt monospace'
+c.fonts.statusbar = '14pt monospace'
+c.fonts.tabs.selected = '14pt monospace'
+c.fonts.tabs.unselected = '14pt monospace'
+c.fonts.contextmenu = '14pt monospace'
+c.qt.args=["blink-settings=darkMode=4"]
+
+c.content.headers.accept_language = "ja-JP,js;q=0.9"
+c.url.searchengines = { "DEFAULT" : "https://searx.info/search?q={}" }
+
+# c.content.user_stylesheets = "~/software/solarized-everything-css/css/apprentice/apprentice-all-sites.css"
+# config.bind(',ap', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/apprentice/apprentice-all-sites.css ""')
+# config.bind(',dr', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/darculized/darculized-all-sites.css ""')
+# config.bind(',gr', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/gruvbox/gruvbox-all-sites.css ""')
+# config.bind(',sd', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css ""')
+# config.bind(',sl', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/solarized-light/solarized-light-all-sites.css ""')
+
+c.zoom.default = '110%'
+c.url.default_page = 'https://searx.info'
+c.url.start_pages = ['https://searx.info']
+c.tabs.background = True
+
+c.colors.webpage.preferred_color_scheme = 'dark'
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+c.colors.webpage.darkmode.contrast = 0.0
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.grayscale.all = False
+c.colors.webpage.darkmode.grayscale.images = 0.0
+c.colors.webpage.darkmode.policy.images = 'smart'
+c.colors.webpage.darkmode.policy.page = 'smart'
+c.colors.webpage.darkmode.threshold.background = 128
+c.colors.webpage.darkmode.threshold.text = 128 
+
 c.qt.args = ['disable-seccomp-filter-sandbox']
