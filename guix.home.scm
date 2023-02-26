da52354c5ee332084bbb1fbc0a979801389b5b67
chris <chris@bumblehead.com>
2023-02-25 23:52:53 -0800

    slightly smaller home config

--- guix.home.scm
+++ guix.home.scm
@@ -1,9 +1,3 @@
-;; This "home-environment" file can be passed to 'guix home reconfigure'
-;; to reproduce the content of your profile.  This is "symbolic": it only
-;; specifies package names.  To reproduce the exact same profile, you also
-;; need to capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
-
 (use-modules (gnu home)
              (gnu home services)
              (gnu home services shells)
@@ -19,8 +13,7 @@
 (define anki
   (load "guix.package.anki-bin.scm"))
 
-(primitive-load (string-append (dirname (current-filename))
-                               "/guix.common.scm"))
+(primitive-load "guix.common.scm")
 
 (define %xdg-config-files
   `(("foot.ini" . "foot/foot.ini")
