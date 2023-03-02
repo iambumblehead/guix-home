0760dc411a271895f20a35d514f6b556e15221fc
chris <chris@bumblehead.com>
2023-03-02 01:41:39 -0800

    update anki bin package file

--- guix.package.anki-bin.scm
+++ guix.package.anki-bin.scm
@@ -3,43 +3,44 @@
   #:use-module (gnu packages gcc)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix gexp)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (guix download))
 
-;;; how to run a dynamically compiled ... stackexchange
 (package
  (name "anki")
- (version "2.1.58")
+ (version "2.1.60")
  (home-page "https://apps.ankiweb.net/")
  (synopsis "Powerful, intelligent flash cards")
  (description "Anki is a program which makes remembering ...")
  (license license:agpl3+)
- (source (local-file "/home/bumble/software/anki/anki"))
+ (source
+  (local-file "/home/bumble/software/anki" "anki" #:recursive? #t))
+ ;;
+ ;; (source (local-dir "/home/bumble/software/anki/anki"))
+ ;;
+ ;; source expects tar.gz and tar.zst from github results in error
+ ;;
+ ;; (source
+ ;;  (origin
+ ;;   (method url-fetch)
+ ;;   (uri (string-append
+ ;;         "https://github.com/ankitects/anki/releases/download/"
+ ;;         version "/anki-" version "-linux-qt6.tar.zst"))
+ ;;   (sha256
+ ;;    (base32 "0dfdcxjjppc96sf5syhnpgdd1825xlynfphvn7d18r0sqxa0hy11"))))
  (supported-systems '("x86_64-linux"))
  (build-system binary-build-system)
  (arguments
   `(#:patchelf-plan
     `(("anki" ("libc" "gcc:lib")))
     #:install-plan
-    `(("anki" "bin/"))
+    `(("anki" "bin/")
+      ("lib" "bin/lib"))
     #:phases
     (modify-phases %standard-phases
                    (add-after 'unpack 'chmod-to-allow-patchelf
                               (lambda _
                                 (chmod "anki" #o755)))
-                   ;;;(add-after 'install 'make-wrapper
-                   ;;;           (lambda* (#:key inputs outputs #:allow-other-keys)
-                   ;;;                    (let* ((out (assoc-ref outputs "out"))
-                   ;;;                           (wrapper (string-append out "/bin/beglitched"))
-                   ;;;                           (binary (string-append out "/share/beglitched/" ,binary)))
-                   ;;;                      (make-wrapper
-                   ;;;                       wrapper binary
-                   ;;;                       `("PATH" ":" prefix
-                   ;;;                         (,(string-append (assoc-ref inputs "pulseaudio") "/bin")))))
-                   ;;;                    #t))
                    )))
  (inputs
   `(("gcc:lib" ,gcc "lib"))))
-;;; note how the first string must match the one in
-;;; patchelf-plan. libc is already implicitely defined
-;;;
-;;; guix build -s x86_64-linux -f my-package.scm
