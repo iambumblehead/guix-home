c7b7ac5a733c8f498b892aacbedef6fa575924f1
chris <chris@bumblehead.com>
2023-08-24 21:48:39 -0700

    update erc fill window hook

--- emacs-erc.el
+++ emacs-erc.el
@@ -1,17 +1,11 @@
-;(make-variable-buffer-local 'erc-fill-column)
-;(add-hook 'window-configuration-change-hook 
-;	        '(lambda ()
-;	           (save-excursion
-;	             (walk-windows
-;		            (lambda (w)
-;		              (let ((buffer (window-buffer w)))
-;		                (set-buffer buffer)
-;		                (when (eq major-mode 'erc-mode)
-;		                  (setq erc-fill-column (- (window-width w) 2)))))))))
-
-(add-hook 'window-configuration-change-hook 
+(add-hook 'window-configuration-change-hook
           '(lambda ()
-	           (setq erc-fill-column (- (window-width) 2))))
+             (save-excursion
+               (walk-windows
+                (lambda (w)
+                  (let ((major (with-current-buffer (window-buffer w) major-mode)))
+                    (when (eq major 'erc-mode)
+                      (setq erc-fill-column (- (window-width w) 2)))))))))
 
 (defmacro unpack-color (color red green blue &rest body)
   `(let ((,red   (car ,color))
