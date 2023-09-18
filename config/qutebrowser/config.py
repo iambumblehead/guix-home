d06e687f5702f46392214a3afbbcc2a86f2fb16d
chris <chris@bumblehead.com>
2023-09-18 00:56:41 -0700

    change default search

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -20,7 +20,7 @@ c.qt.args=["blink-settings=darkMode=4"]
 c.content.cookies.accept = 'no-3rdparty'
 c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 c.content.headers.accept_language = "ja-JP,js;q=0.9"
-c.url.searchengines = { "DEFAULT" : "https://searx.info/search?q={}" }
+c.url.searchengines = { "DEFAULT" : "https://searx.envs.net/searxng/search?q={}" }
 
 # c.content.user_stylesheets = "~/software/solarized-everything-css/css/apprentice/apprentice-all-sites.css"
 # config.bind(',ap', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/apprentice/apprentice-all-sites.css ""')
@@ -30,8 +30,8 @@ c.url.searchengines = { "DEFAULT" : "https://searx.info/search?q={}" }
 # config.bind(',sl', 'config-cycle content.user_stylesheets ~/software/solarized-everything-css/css/solarized-light/solarized-light-all-sites.css ""')
 
 c.zoom.default = '80%'
-c.url.default_page = 'https://searx.info'
-c.url.start_pages = ['https://searx.info']
+c.url.default_page = 'https://searx.envs.net'
+c.url.start_pages = ['https://searx.envs.net']
 c.tabs.background = True
 
 c.colors.webpage.preferred_color_scheme = 'dark'
